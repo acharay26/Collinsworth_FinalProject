3770e297b271fc239fd94d204108e9aed7a95ea4
acharay26 <146345606+acharay26@users.noreply.github.com>
2023-11-28 12:34:39 -0500

    Add files via upload

---
+++ acharay_Assignment01/src/Main.py
@@ -0,0 +1,15 @@
+# Name:Aditya Achar
+# email: acharay@mail.uc.edu
+# Assignment Title: Assignment_01
+# Due Date: 08/29/2023
+# Course: IS 4010
+# Semester/Year: Fall 2023
+# Brief Description: This project demonstrates that I can use Eclipse to create a PyDev project...
+# Citations:
+# Anything else that's relevant:
+
+print("Hello World")
+
+pot = 3456
+
+print(type(pot))
